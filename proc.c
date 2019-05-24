bc2b287dc910cf1f61c2ddc8102e480f3174d8d6
psemenov <sp17september@gmail.com>
2019-05-24 20:24:15 +0300

    alter proc.c

--- proc.c
+++ proc.c
@@ -14,36 +14,33 @@ void synchronize(process *proc, MessageType m_type, char *payload, size_t payloa
     Message tmp_msg = init_msg(m_type , payload_len);
     memcpy(tmp_msg.s_payload, payload, payload_len);
     send_multicast((void*)proc, (const Message *)&tmp_msg);
-    for (size_t i = 1; i <= proc_number; i++) {
-       if (i != proc->id) {
-           while(receive((void*)proc, i, &tmp_msg) != 0);
-           set_lamport_time(tmp_msg.s_header.s_local_time);
+    /* new part */
+    int total = proc_number - 1;
+    while (total > 0) {
+        while (receive_any((void*)proc, &tmp_msg < 0));
+        set_lamport_time(tmp_msg.s_header.s_local_time);
            inc_time();
-       }
+        if(m_type == tmp_msg.s_header.s_type) --total; 
     }
 }
 
 
 
 void working_cycle(process *proc) {
-    while (true) {
-        Message tmp_msg = {{ 0 }};
-        if (receive_any(proc, &tmp_msg) < 0) continue;
-        set_lamport_time(tmp_msg.s_header.s_local_time);
-        inc_time();
 
-        if(tmp_msg.s_header.s_type == TRANSFER){
-            fprintf(pipe_log,"PID %d received TRANSFER message.\n",proc->id);
-            transfer_cycle(proc, balance_history, (TransferOrder*)(tmp_msg.s_payload));
-        }else if(tmp_msg.s_header.s_type == STOP){
-            fprintf(pipe_log,"PID %d received STOP message.\n",proc->id);
-            return;
-        } else {
-            fprintf(pipe_log,"PID %d received wrong message with m_type %d.\n",
-                proc->id, tmp_msg.s_header.s_type);
-            continue;
-        }
+    if (proc->io->mutexl)
+        request_cs((void*)proc);
+    
+    for (int i = 1; i <= 5 * proc->id; i++) {
+        size_t slen = snprintf(NULL, 0, log_loop_operation_fmt, proc->id, i,
+                               5 * proc->id);
+        char *str = (char*)alloca(slen + 1);
+        snprintf(str, slen+1, log_loop_operation_fmt, proc->id, i, 5 * proc->id);
+        print(str);
     }
+
+    if (proc->io->mutexl)
+        release_cs((void*)proc);
 }
 
 /** Child main function. */ 
